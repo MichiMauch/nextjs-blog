262d34f63e70eb4acfe4bcd928526d28d6e84688
MichiMauch <michi.mauch@gmail.com>
2024-04-04 22:23:09 +0200

    first external component added

--- src/app/_components/post-title.tsx
+++ src/app/_components/post-title.tsx
@@ -1,13 +1,29 @@
+"use client"; // This is a client component 👈🏽
+
 import { ReactNode } from "react";
+import { TypeAnimation } from 'react-type-animation';
+import { useState } from "react";
 
 type Props = {
   children?: ReactNode;
 };
 
 export function PostTitle({ children }: Props) {
+  // Typzusicherung, dass children ein String ist
+  const text = typeof children === 'string' ? children : '';
+
   return (
-    <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
-      {children}
-    </h1>
+    <div className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <TypeAnimation
+      sequence={[
+        text,
+        1000, // Wartezeit
+      ]}
+      wrapper="h1"
+      speed={50}
+      //style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+    </div>
   );
 }
